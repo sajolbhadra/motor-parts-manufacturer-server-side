8a14af13db58071e2d19f8b39c37ad2a20c0a694
sajolbhadra <sajolbhadra@gmail.com>
2022-05-24 03:53:42 +0600

    mongodb connect and readme updated

---
+++ README.md
@@ -0,0 +1,20 @@
+# Website [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+### `Website Features`
+*Interactive User Interface
+*
+
+
+### `User Technologies`
+* React
+* Tailwind with Daisy UI
+* Firebase Authentication
+* React Router DOM v6
+* Environmental Variable
+* React Firebase Hooks
+* React Hook Form
+* React Query
+* Firebase Hosting
+* Heroku for Server Deployment
--- index.js
+++ index.js
@@ -4,7 +4,29 @@ const app = express();
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const ObjectId = require('mongodb').ObjectId;
 
+//middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.60qwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try{
+        await client.connect();
+        
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('Server Online');
