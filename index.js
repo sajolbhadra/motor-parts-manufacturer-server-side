2818e3872badc3ae9b76db95ede50f8ada12579d
sajolbhadra <sajolbhadra@gmail.com>
2022-05-24 03:59:25 +0600

    CRUD operation on item

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const app = express();
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
 
+
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const ObjectId = require('mongodb').ObjectId;
 
+
 //middleware
 app.use(cors())
 app.use(express.json())
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.60qwo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -19,7 +22,61 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try{
         await client.connect();
-        
+        console.log('server online')
+        const itemsCollection = client.db('electricalworks').collection('items'); 
+        const usersCollection = client.db('electricalworks').collection('users'); 
+
+        //---------Insert an Item------------
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            console.log('new item added', newItem);
+            const result= await itemsCollection.insertOne(newItem);
+            // res.send({result : 'success'})
+            res.send(result)
+        });
+
+        //---------Get all items----------
+        app.get('/item', async (req, res) => {
+            const query ={};
+            const cursor = itemsCollection.find(query)
+            const items = await cursor.toArray();
+            res.send(items);
+        })
+
+        //--- Get Individual Item Details-------------
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await itemsCollection.findOne(query);
+            res.send(result)
+        })
+
+        //-----Update Item-----------
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateItem = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = {upsert: true};
+            const updateDoc ={
+                $set: {
+                    quantity:updateItem.quantity
+                }
+            };
+            const result = await itemsCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+            console.log(result)
+        })
+
+        //--------Delete Individual Item---------------
+        app.delete('/item/:id', async(req, res) => {
+            const id = req.params.id;
+
+            const query = {_id: ObjectId(id)};
+            const result = await itemsCollection.deleteOne(query);
+            res.send(result);
+            console.log(id, result)
+        })
+
     }
     finally{
         // await client.close();
